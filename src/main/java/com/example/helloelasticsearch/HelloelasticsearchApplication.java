df94c0bb8172f33032059a7ad099193a6de85fa3
zhouhongzhan <zhz666521@163.com>
2020-07-26 14:09:31 +0800

    ElasticSearch Reposity Create

---
+++ src/main/java/com/example/helloelasticsearch/HelloelasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloelasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloelasticsearchApplication.class, args);
+    }
+
+}
